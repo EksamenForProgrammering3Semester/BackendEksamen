b58b9be08c2fb40468cf30cd765bf434b333d00e
Hassan2200dk <hassan.2002@outlook.dk>
2024-01-09 16:10:45 +0100

    Initial commit

---
+++ src/main/java/com/example/backendfor3semester/BackendFor3SemesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendfor3semester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFor3SemesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendFor3SemesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendfor3semester/BackendFor3SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendfor3semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFor3SemesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
