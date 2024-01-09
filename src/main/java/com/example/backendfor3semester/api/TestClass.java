b45fcdce9ae6d3921bf270d86673fe44f158bf4c
Hassan2200dk <hassan.2002@outlook.dk>
2024-01-09 16:10:45 +0100

    Initial commit

---
+++ src/main/java/com/example/backendfor3semester/api/TestClass.java
@@ -0,0 +1,17 @@
+package com.example.backendfor3semester.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestClass {
+
+    @GetMapping()
+    public String getname(){
+        return "hej";
+    }
+    @GetMapping("all")
+    public String getnameall(){
+        return "hej all";
+    }
+}
